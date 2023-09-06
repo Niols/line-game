d99eefa47f1c3c738656e3503f6f67121dc7119c
Niols <niols@niols.fr>
2023-09-06 20:36:03 +0000

    Fancier cards dragging

--- client/src/scenes/game.js
+++ client/src/scenes/game.js
@@ -50,6 +50,26 @@ export default class Game extends Phaser.Scene {
         this.zone = new Zone(this);
         this.dropZone = this.zone.renderZone();
         this.outline = this.zone.renderOutline(this.dropZone);
+
+        this.input.on('dragstart', function (pointer, gameObject) {
+            gameObject.setTint(0xff69b4);
+            self.children.bringToTop(gameObject);
+        })
+
+        this.input.on('dragend', function (pointer, gameObject, dropped) {
+            gameObject.setTint();
+            if (!dropped) {
+                gameObject.x = gameObject.input.dragStartX;
+                gameObject.y = gameObject.input.dragStartY;
+            }
+        })
+
+        this.input.on('drop', function (pointer, gameObject, dropZone) {
+            dropZone.data.values.cards++;
+            gameObject.x = (dropZone.x - 350) + (dropZone.data.values.cards * 50);
+            gameObject.y = dropZone.y;
+            gameObject.disableInteractive();
+        })
     }
 
     update() {
