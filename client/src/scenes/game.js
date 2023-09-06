03baf932fbce6b6108aac457aac1d515b3bd16a1
Niols <niols@niols.fr>
2023-09-06 20:01:55 +0000

    Work on the `create` function

--- client/src/scenes/game.js
+++ client/src/scenes/game.js
@@ -14,6 +14,32 @@ export default class Game extends Phaser.Scene {
 
     create() {
         this.dealText = this.add.text(75, 350, ['DEAL CARDS']).setFontSize(18).setFontFamily('Trebuchet MS').setColor('#00ffff').setInteractive();
+
+        let self = this;
+
+        this.card = this.add.image(300, 300, 'cyanCardFront').setScale(0.3, 0.3).setInteractive();
+        this.input.setDraggable(this.card);
+
+        this.dealCards = () => {
+
+        }
+
+        this.dealText.on('pointerdown', function () {
+            self.dealCards();
+        })
+
+        this.dealText.on('pointerover', function () {
+            self.dealText.setColor('#ff69b4');
+        })
+
+        this.dealText.on('pointerout', function () {
+            self.dealText.setColor('#00ffff');
+        })
+
+        this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+            gameObject.x = dragX;
+            gameObject.y = dragY;
+        })
     }
 
     update() {
